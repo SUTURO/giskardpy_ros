e432e3de0030e8f9d6b5740273d9554d3288b6bc
K3cks <philk99@web.de>
2023-03-02 15:31:30 +0100

    cleaned polling

--- src/giskardpy/tree/behaviors/suturo_world_syncroniser.py
+++ src/giskardpy/tree/behaviors/suturo_world_syncroniser.py
@@ -3,13 +3,15 @@ from geometry_msgs.msg import PoseStamped, Pose
 from giskard_msgs.srv import UpdateWorldRequest, UpdateWorld
 from py_trees import Status
 
-from giskardpy.model.utils import make_world_body_box
+from giskardpy.model.utils import make_world_body_box, make_world_body_cylinder, make_world_body_sphere
 from giskardpy.tree.behaviors.plugin import GiskardBehavior
 
 from rosprolog_client import Prolog
 
 import pydevd_pycharm
-#pydevd_pycharm.settrace('localhost', port=1234, stdoutToServer=True, stderrToServer=True, suspend=False)
+
+
+# pydevd_pycharm.settrace('localhost', port=1234, stdoutToServer=True, stderrToServer=True, suspend=False)
 
 
 class SuturoWorldSynchroniser(GiskardBehavior):
@@ -51,14 +53,30 @@ class SuturoWorldSynchroniser(GiskardBehavior):
         self.last_update = updates['CurrentTime']
         print(updates, flush=True)
 
-        # TODO Add objects to giskard
         for element in updates['Updates']:
-            #self.world.root_link_name
-            #self.world.groups
+            # self.world.root_link_name
+            # self.world.groups
+
+            obj = element[3]['term']
 
             obj_name = str(element[2][0])
-            obj_parent_link_group = ''
-            obj_parent_link = ''
+
+            # Create object body
+
+            if obj[0] == 'box':
+                obj_body = make_world_body_box(obj[1], obj[2], obj[3])
+                print("added box")
+            elif obj[0] == 'cylinder':
+                obj_body = make_world_body_cylinder(obj[2], obj[1])
+                print('added cylinder')
+            elif obj[0] == 'sphere':
+                obj_body = make_world_body_sphere(obj[1])
+                print('added sphere')
+            else:
+                print("No object body to add")
+                continue
+
+            # Object pose
             obj_pose = Pose()
             obj_pose.position.x = element[2][1][0]
             obj_pose.position.y = element[2][1][1]
@@ -68,25 +86,12 @@ class SuturoWorldSynchroniser(GiskardBehavior):
             obj_pose.orientation.z = element[2][2][2]
             obj_pose.orientation.w = element[2][2][3]
 
-            timeout = 2.0
-            obj = element[3]['term']
-            print(obj)
+            # Parent link
+            obj_parent_link_group = ''
+            obj_parent_link = ''
+            parent_link = self.world.get_link_name(obj_parent_link, obj_parent_link_group)
 
-            req = UpdateWorldRequest()
-
-            req.group_name = obj_name
-            req.operation = UpdateWorldRequest.ADD
-            req.timeout = timeout
-            if obj[0] == 'box':
-                req.body = make_world_body_box(obj[1], obj[2], obj[3])
-                print("box added")
-            req.parent_link_group = obj_parent_link_group
-            req.parent_link = self.world.get_link_name(obj_parent_link, obj_parent_link_group)
-            req.pose = obj_pose
-
-            self.world.add_world_body(obj_name, req.body, req.pose, req.parent_link)
-            #self._update_world_srv.call(req)
-
+            self.world.add_world_body(obj_name, obj_body, obj_pose, parent_link)
 
     @profile
     def update(self):
