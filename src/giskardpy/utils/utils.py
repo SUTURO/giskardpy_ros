962676e92e5a7ddbec34e97129378e53ceb94a95
K3cks <philk99@web.de>
2023-09-28 12:26:16 +0200

    parsing now inside of convert_ros_message

--- src/giskardpy/utils/utils.py
+++ src/giskardpy/utils/utils.py
@@ -369,14 +369,33 @@ def make_pose_from_parts(pose, frame_id, position, orientation):
     return pose
 
 
-def convert_ros_message_to_dictionary(message: Message) -> dict:
-    type_str_parts = str(type(message)).split('.')
-    part1 = type_str_parts[0].split('\'')[1]
-    part2 = type_str_parts[-1].split('\'')[0]
-    message_type = f'{part1}/{part2}'
-    d = {'message_type': message_type,
-         'message': original_convert_ros_message_to_dictionary(message)}
-    return d
+def convert_ros_message_to_dictionary(message) -> dict:
+
+    if isinstance(message, list):
+        for i, element in enumerate(message):
+            message[i] = convert_ros_message_to_dictionary(element)
+
+    elif isinstance(message, dict):
+        for k, v in message.copy().items():
+            message[k] = convert_ros_message_to_dictionary(v)
+
+    elif isinstance(message, tuple):
+        vlist = list(message)
+        for i, element in enumerate(vlist):
+            vlist[i] = convert_ros_message_to_dictionary(element)
+        message = tuple(vlist)
+
+    elif isinstance(message, Message):
+
+        type_str_parts = str(type(message)).split('.')
+        part1 = type_str_parts[0].split('\'')[1]
+        part2 = type_str_parts[-1].split('\'')[0]
+        message_type = f'{part1}/{part2}'
+        d = {'message_type': message_type,
+             'message': original_convert_ros_message_to_dictionary(message)}
+        return d
+
+    return message
 
 
 def replace_prefix_name_with_str(d: dict) -> dict:
