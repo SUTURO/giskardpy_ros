c876942fa76851be11100e8fd0ec7f288a8f7fa8
MideDay <l-hauschild@outlook.de>
2023-11-24 13:39:18 +0100

    open/close gripper implementation moved from suturo.py to python_interface.py

--- src/giskardpy/python_interface.py
+++ src/giskardpy/python_interface.py
@@ -7,12 +7,15 @@ import geometry_msgs.msg
 import rospy
 from actionlib import SimpleActionClient
 from actionlib_msgs.msg import GoalStatus
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
 from genpy import Message
 from geometry_msgs.msg import PoseStamped, Vector3Stamped, PointStamped, QuaternionStamped, Vector3
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from rospy import ServiceException
 from sensor_msgs.msg import JointState
 from shape_msgs.msg import SolidPrimitive
+from tmc_control_msgs.msg import GripperApplyEffortAction, GripperApplyEffortGoal
+from trajectory_msgs.msg import JointTrajectoryPoint
 from visualization_msgs.msg import MarkerArray
 
 from giskard_msgs.msg import MoveAction, MoveGoal, WorldBody, CollisionEntry, MoveResult, Constraint, \
@@ -1130,3 +1133,47 @@ class GiskardWrapper:
         action_state = cli.get_state()
         if action_state == GoalStatus.SUCCEEDED:
             rospy.loginfo("Navigation Succeeded")
+
+    def open_gripper(self):
+        self.move_gripper_force(0.8)
+
+    def close_gripper(self):
+        self.move_gripper_force(-0.8)
+
+    def neutral_gripper(self):
+        self.set_gripper_joint_position(0.5)
+
+    def move_gripper_force(self, force: float = 0.8):
+        """
+        Closes the gripper with the given force.
+        :param force: force to grasp with should be between 0.2 and 0.8 (N)
+        :return: applied effort
+        """
+        _gripper_apply_force_client = actionlib.SimpleActionClient('/hsrb/gripper_controller/grasp',
+                                                                   GripperApplyEffortAction)
+
+        rospy.loginfo("Closing gripper with force: {}".format(force))
+        f = force  # max(min(0.8, force), 0.2)
+        goal = GripperApplyEffortGoal()
+        goal.effort = f
+        _gripper_apply_force_client.send_goal(goal)
+
+    def set_gripper_joint_position(self, position):
+        """
+        Sets the gripper joint to the given  position
+        :param position: goal position of the joint -0.105 to 1.239 rad
+        :return: error_code of FollowJointTrajectoryResult
+        """
+        _gripper_controller = actionlib.SimpleActionClient('/hsrb/gripper_controller/follow_joint_trajectory',
+                                                           FollowJointTrajectoryAction)
+
+        pos = max(min(1.239, position), -0.105)
+        goal = FollowJointTrajectoryGoal()
+        goal.trajectory.joint_names = [u'hand_motor_joint']
+        p = JointTrajectoryPoint()
+        p.positions = [pos]
+        p.velocities = [0]
+        p.effort = [0.1]
+        p.time_from_start = rospy.Time(1)
+        goal.trajectory.points = [p]
+        _gripper_controller.send_goal(goal)
