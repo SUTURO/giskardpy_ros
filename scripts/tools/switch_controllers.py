ae0fde0933cfac5670850ad4ee32a38c2899d524
MideDay <l-hauschild@outlook.de>
2024-03-21 13:54:09 +0100

    added 5sec timer between changes

--- scripts/tools/switch_controllers.py
+++ scripts/tools/switch_controllers.py
@@ -7,10 +7,11 @@ from sensor_msgs.msg import Joy
 class SwitchControllers:
 
     def __init__(self):
-        rospy.init_node("~/switch_controllers")
+        rospy.init_node("switch_controllers")
 
         self.data = Joy()
 
+        self.last_change: float = 0
         rospy.wait_for_service('/hsrb/controller_manager/list_controllers')
         self.srv_list_con = rospy.ServiceProxy(name='/hsrb/controller_manager/list_controllers',
                                                service_class=ListControllers)
@@ -21,7 +22,10 @@ class SwitchControllers:
         self.sub_joy = rospy.Subscriber(name='/hsrb/joy', data_class=Joy, callback=self.testing_cb)
 
     def testing_cb(self, data: Joy):
-        if data.buttons[7] == 1 and data.buttons[8] == 1 and self.check_controllers():
+        time_diff = data.header.stamp.secs - self.last_change
+
+        if data.buttons[7] == 1 and data.buttons[8] == 1 and self.check_controllers() and time_diff > 5.0:
+            self.last_change = data.header.stamp.sec
             self.switch_controls()
 
     def check_controllers(self):
@@ -43,16 +47,16 @@ class SwitchControllers:
 
         controller_dict = {controller.name: controller for controller in resp.controller}
 
+        strictness: int = 2
+        start_asap: bool = False
+        timeout: float = 0.0
+
         if (controller_dict['arm_trajectory_controller'].state == 'running'
                 and controller_dict['head_trajectory_controller'].state == 'running'
                 and (controller_dict['realtime_body_controller_real'].state == 'stopped'
                      or controller_dict['realtime_body_controller_real'].state == 'initialized')):
-            print('test')
             start_con = ['realtime_body_controller_real']
             stop_con = ['arm_trajectory_controller', 'head_trajectory_controller']
-            strictness: int = 2
-            start_asap: bool = False
-            timeout: float = 0.0
             resp: SwitchControllerResponse = self.srv_switch_con(start_con, stop_con, strictness, start_asap, timeout)
 
         elif ((controller_dict['arm_trajectory_controller'].state == 'stopped'
@@ -60,15 +64,9 @@ class SwitchControllers:
               and (controller_dict['head_trajectory_controller'].state == 'stopped'
                    or controller_dict['head_trajectory_controller'].state == 'initialized')
               and controller_dict['realtime_body_controller_real'].state == 'running'):
-            print('test2')
             start_con = ['arm_trajectory_controller', 'head_trajectory_controller']
             stop_con = ['realtime_body_controller_real']
-            strictness = 2
-            start_asap: bool = False
-            timeout: float = 0.0
             resp: SwitchControllerResponse = self.srv_switch_con(start_con, stop_con, strictness, start_asap, timeout)
-        else:
-            print('test3')
 
 
 if __name__ == '__main__':
