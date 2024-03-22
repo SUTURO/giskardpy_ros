5c80d7aba5f13f3b551430b98090bf42d605933e
Yannis Bülter <yannis-buelter@gmx.de>
2024-03-22 12:30:43 +0100

    removed / before a topic in the hsr.py, since it led to errors on the real hsr which caused it to search for the topic "//hsrb/realtime_body_controller_real" instead of "/hsrb/realtime_body_controller_real"

--- src/giskardpy/configs/iai_robots/hsr.py
+++ src/giskardpy/configs/iai_robots/hsr.py
@@ -124,7 +124,7 @@ class HSRVelocityInterface(RobotInterfaceConfig):
         self.sync_joint_state_topic('/hsrb/joint_states')
         self.sync_odometry_topic('/hsrb/odom', self.drive_joint_name)
 
-        self.add_joint_velocity_group_controller(namespace='/hsrb/realtime_body_controller_real')
+        self.add_joint_velocity_group_controller(namespace='hsrb/realtime_body_controller_real')
 
         self.add_base_cmd_velocity(cmd_vel_topic='/hsrb/command_velocity',
                                    joint_name=self.drive_joint_name)
