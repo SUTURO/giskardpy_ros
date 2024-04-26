202f694bb2ef7da55aedc24162f99386260ca058
MideDay <l-hauschild@outlook.de>
2024-04-26 12:38:01 +0200

    fixed imports after merge

--- src/giskardpy/goals/goal.py
+++ src/giskardpy/goals/goal.py
@@ -4,11 +4,6 @@ import abc
 from abc import ABC
 from typing import Dict, List, Union
 
-from giskardpy.monitors.monitors import ExpressionMonitor, Monitor
-from giskardpy.god_map import god_map
-from giskardpy.symbol_manager import symbol_manager
-from giskardpy.tasks.task import Task
-from giskardpy.utils.utils import string_shortener
 import controller_manager_msgs
 # import giskardpy.identifier as identifier
 import rospy
@@ -21,10 +16,9 @@ from giskardpy.exceptions import GiskardException
 from giskardpy.exceptions import GoalInitalizationException
 from giskardpy.god_map import god_map
 from giskardpy.model.joints import OneDofJoint
-from giskardpy.data_types import PrefixName, Derivatives
-from giskardpy.qp.constraint import InequalityConstraint, EqualityConstraint, DerivativeInequalityConstraint
-import giskardpy.casadi_wrapper as cas
 from giskardpy.monitors.monitors import ExpressionMonitor
+from giskardpy.monitors.monitors import Monitor
+from giskardpy.symbol_manager import symbol_manager
 from giskardpy.tasks.task import Task
 from giskardpy.tree.behaviors.suturo_monitor_force_sensor import MonitorForceSensor
 # from giskardpy.tree.garden import success_is_failure
