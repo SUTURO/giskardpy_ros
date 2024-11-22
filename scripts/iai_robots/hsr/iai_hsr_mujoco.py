c519d8452d8cb3c798c771f4e1486ca45b2c8a78
JuliSlz <juschulz@uni-bremen.de>
2024-11-22 17:37:32 +0100

    set_middleware in hsr files

--- scripts/iai_robots/hsr/iai_hsr_mujoco.py
+++ scripts/iai_robots/hsr/iai_hsr_mujoco.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+from giskardpy.middleware import set_middleware
 
 from giskardpy_ros.configs.behavior_tree_config import ClosedLoopBTConfig
 from giskardpy_ros.configs.giskard import Giskard
@@ -7,8 +8,11 @@ from giskardpy_ros.configs.iai_robots.hsr import WorldWithHSRConfig, HSRCollisio
     HSRMujocoVelocityInterface, HSRMujocoPositionInterface
 from giskardpy.qp.qp_controller_config import QPControllerConfig, SupportedQPSolver
 
+from giskardpy_ros.ros1.interface import ROS1Wrapper
+
 if __name__ == '__main__':
     rospy.init_node('giskard')
+    set_middleware(ROS1Wrapper())
     debug_mode = rospy.get_param('~debug_mode', False)
     giskard = Giskard(world_config=WorldWithHSRConfig(),
                       collision_avoidance_config=HSRCollisionAvoidanceConfig(),
--- scripts/iai_robots/hsr/iai_hsr_real_time_with_kitchen.py
+++ scripts/iai_robots/hsr/iai_hsr_real_time_with_kitchen.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 import rospy
+from giskardpy. middleware import set_middleware
 
 from giskardpy_ros.configs.behavior_tree_config import ClosedLoopBTConfig
 from giskardpy_ros.configs.giskard import Giskard
 from giskardpy_ros.configs.iai_robots.hsr import HSRCollisionAvoidanceConfig, \
     SuturoArenaWithHSRConfig, HSRVelocityInterfaceSuturo
+from giskardpy_ros.ros1.interface import ROS1Wrapper
 
 if __name__ == '__main__':
     rospy.init_node('giskard')
+    set_middleware(ROS1Wrapper())
     debug_mode = rospy.get_param('~debug_mode', False)
     environment_name = 'iai_kitchen'
     giskard = Giskard(world_config=SuturoArenaWithHSRConfig(environment_name=environment_name),
