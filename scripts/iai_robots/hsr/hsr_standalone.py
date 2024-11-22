d20acbce71d49c20d0ac5b15c2a7a297f2bcb493
JuliSlz <juschulz@uni-bremen.de>
2024-11-22 17:37:42 +0100

    set_middleware in hsr files

--- scripts/iai_robots/hsr/hsr_standalone.py
+++ scripts/iai_robots/hsr/hsr_standalone.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 import rospy
+from giskardpy.middleware import set_middleware
 
 from giskardpy_ros.configs.behavior_tree_config import StandAloneBTConfig
 from giskardpy_ros.configs.giskard import Giskard
 from giskardpy_ros.configs.iai_robots.hsr import WorldWithHSRConfig, HSRCollisionAvoidanceConfig, \
     HSRStandaloneInterface
+from giskardpy_ros.ros1.interface import ROS1Wrapper
 
 if __name__ == '__main__':
     rospy.init_node('giskard')
+    set_middleware(ROS1Wrapper())
     debug_mode = rospy.get_param('~debug_mode', False)
     giskard = Giskard(world_config=WorldWithHSRConfig(),
                       collision_avoidance_config=HSRCollisionAvoidanceConfig(),
