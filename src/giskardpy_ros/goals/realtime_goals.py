dd093e83b8add412d1459a8130dcdcdac5637c63
Yannis Bülter <yannis-buelter@gmx.de>
2025-02-20 14:59:31 +0100

    added RealTimeConePointing to realtime_goals.py

--- src/giskardpy_ros/goals/realtime_goals.py
+++ src/giskardpy_ros/goals/realtime_goals.py
@@ -14,7 +14,7 @@ import giskardpy_ros.ros1.msg_converter as msg_converter
 from giskardpy.data_types.data_types import PrefixName, Derivatives
 from giskardpy.data_types.exceptions import GoalInitalizationException, ExecutionException
 from giskardpy.goals.goal import Goal
-from giskardpy.goals.pointing import Pointing
+from giskardpy.goals.pointing import Pointing, PointingCone
 from giskardpy.god_map import god_map
 from giskardpy.middleware import get_middleware
 from giskardpy.model.joints import OmniDrive
@@ -45,6 +45,42 @@ class RealTimePointing(Pointing):
                          root_link=root_link,
                          pointing_axis=pointing_axis,
                          max_velocity=max_velocity,
+                         weight=weight,
+                         start_condition=start_condition,
+                         hold_condition=hold_condition,
+                         end_condition=end_condition)
+        self.sub = rospy.Subscriber(topic_name, PointStamped, self.cb)
+
+    def cb(self, data: PointStamped):
+        data = msg_converter.ros_msg_to_giskard_obj(data, god_map.world)
+        data = god_map.world.transform(self.root, data).to_np()
+        self.root_P_goal_point = data
+
+
+class RealTimeConePointing(PointingCone):
+
+    def __init__(self,
+                 tip_link: PrefixName,
+                 root_link: PrefixName,
+                 topic_name: str,
+                 pointing_axis: cas.Vector3,
+                 cone_theta: float = 0.0,
+                 name: Optional[str] = None,
+                 max_velocity: float = 0.3,
+                 threshold: float = 0.01,
+                 weight: float = WEIGHT_BELOW_CA,
+                 start_condition: cas.Expression = cas.TrueSymbol,
+                 hold_condition: cas.Expression = cas.FalseSymbol,
+                 end_condition: cas.Expression = cas.FalseSymbol):
+        initial_goal = cas.Point3((1, 0, 1), reference_frame=god_map.world.search_for_link_name('base_footprint'))
+        super().__init__(name=name,
+                         tip_link=tip_link,
+                         goal_point=initial_goal,
+                         root_link=root_link,
+                         pointing_axis=pointing_axis,
+                         cone_theta=cone_theta,
+                         max_velocity=max_velocity,
+                         threshold=threshold,
                          weight=weight,
                          start_condition=start_condition,
                          hold_condition=hold_condition,
