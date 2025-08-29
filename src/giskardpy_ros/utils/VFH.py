49306f8ffc9f059ba7aadfd1415615fd007fa5a8
MideDay <l-hauschild@outlook.de>
2025-08-29 14:20:06 +0200

    added workflow case for import that fails tests

--- src/giskardpy_ros/utils/VFH.py
+++ src/giskardpy_ros/utils/VFH.py
@@ -1,4 +1,5 @@
 import math
+import os
 import time
 from itertools import groupby
 from operator import itemgetter
@@ -14,7 +15,8 @@ import tf
 
 from giskardpy.utils.math import angle_between_vector
 
-# matplotlib.use('Qt5Agg')
+if 'GITHUB_WORKFLOW' not in os.environ:
+    matplotlib.use('Qt5Agg')
 
 
 class VectorFieldHistogram:
@@ -47,6 +49,7 @@ class VectorFieldHistogram:
         self.sector_angle = sector_angle
         self.obstacle_threshold = obstacle_threshold
         self.s_max = s_max
+        # self.human_point = human_point
         self.direction_vector = None
 
         self.topic = input_topic  # "/hsrb/base_scan"
@@ -62,50 +65,34 @@ class VectorFieldHistogram:
         self.sub = rospy.Subscriber(self.topic, LaserScan, self.laser_callback)
         self.rate = rospy.Rate(10)
 
-    # calculate polar obstacle density, as indicator how many obstacles are within a sector - tbd
-    # Unit testing by checking steering angles - tbd
+        # used to limit interval of plot generation
+        self.plotting = 0
+        self.frequency = 50
+
+    # ideas: check whether human_point is behind obstacle; use that as part of condition to start search algo
+    # - make sure there are always more than one valleys in search stage; check which valley is closer to the direction of the human point(??)
+    # TODO: Unit testing by checking steering angles
 
     def laser_callback(self, data: LaserScan):
         self.distances = np.array(data.ranges)
-        self.angles = data.angle_min + np.arange(len(self.distances)) * data.angle_increment
-
-        # self.laser_points_x = []
-        # self.laser_points_y = []
-        #
-        # for i, r in enumerate(self.distances):
-        #     if np.isnan(r) or np.isinf(r):
-        #         continue
-        #
-        #     angle = self.angles[i]
-        #
-        #     point = PointStamped()
-        #     point.header.frame_id = data.header.frame_id
-        #     point.header.stamp = data.header.stamp
-        #     point.point.x = np.cos(angle)
-        #     point.point.y = np.sin(angle)
-        #     point.point.z = 0.0
-        #
-        #     try:
-        #         transformed_point = self.tf_listener.transformPoint("map", point)
-        #         self.laser_points_x.append(transformed_point.point.x)
-        #         self.laser_points_y.append(transformed_point.point.y)
-        #     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        #         continue
-        #
-        #     self.x_points = np.array(self.laser_points_x)
-        #     self.y_points = np.array(self.laser_points_y)
+        self.angles = np.arange(len(self.distances)) * data.angle_increment
 
     def target_sim(self,
                    target_point=(1.0, 0.0, 0.0)):
         # start_time = time.perf_counter() - benchmark stuff
         # calculating sector that target is in
+        target_point[2] = 0
         target_angle = angle_between_vector(v1=np.array([1, 0, 0]), v2=target_point) + 2.0944
+        if target_point[1] > 0:
+            target_angle = -target_angle
 
         # print('target_angle:', target_angle)
         target_angle_deg = np.rad2deg(target_angle) % 240
         # print('target_angle_deg:', target_angle_deg)
         target_sector = int(target_angle_deg // self.sector_angle)
         print(f"Target Sector:{target_sector}")
+        # calculate sector that human_point is in and pass it to update to use as req for search algo
+
         # end_time = time.perf_counter()  # End timing
         # elapsed_time_ms = (end_time - start_time) * 1000
         # rospy.loginfo(f"target_sim took {elapsed_time_ms:.3f} ms") - benchmark stuff
@@ -118,7 +105,11 @@ class VectorFieldHistogram:
         # print(f'target_point: {target_point}')
         target_sector, target_angle_deg, target_point = self.target_sim(target_point=target_point)
         self.update_histogram(self.distances, self.angles, target_sector, target_angle_deg)
-        # self.histogram_plot(target_point)
+        if self.plotting >= self.frequency:
+            self.histogram_plot(target_point)
+            self.plotting = 0
+        else:
+            self.plotting += 1
 
     def update_histogram(self, distances, angles, target_sector, target_angle_deg):
         """
@@ -132,7 +123,7 @@ class VectorFieldHistogram:
         Should the sector of the target direction coincide with a sector that's part of a valley,
         then the robot will be pushed into the direction of a target. Should the target be obstructed
         by an obstacle, the algorithm will search for the nearest available valley of a certain size to
-        autonomously avoid the obstacle.
+        autonomously avoid said obstacle.
 
         :param distances: distances from the target sector
         :param angles: angles from the respective laser scanner reading
@@ -164,12 +155,11 @@ class VectorFieldHistogram:
             sector_dists = distances[mask]
             # density = np.sum((self.max_range - sector_dists) / self.max_range)
             clipped_dists = np.clip(sector_dists, 0, self.d_max)
-            # replace formula so that distance is between max.range and 0, whereas max.range is nearest to the robot
             magnitudes = 1 - (clipped_dists / self.d_max)
             polar_histogram[sector] = np.sum(magnitudes)
         polar_histogram = polar_histogram[::-1]  # flips histogram, because we have a right hand coord system
-        print(polar_histogram)
-
+        # print(polar_histogram)
+        # TODO: Look into smoothing the polar histogram
         # polar_histogram = self.smooth_polar_histogram(polar_histogram, l=5)
         # print(f'Magnitude Sum for Sector {sector}:{np.sum(magnitudes)}')
         free_sectors = np.where(polar_histogram < self.obstacle_threshold)[0]
@@ -179,34 +169,41 @@ class VectorFieldHistogram:
         for k, g in groupby(enumerate(free_sectors), lambda ix: ix[0] - ix[1]):
             valley = list(map(itemgetter(1), g))
             valleys.append(valley)
-            # print(f"Valley: {valley}")
-            # print(f"Valleys: {valleys}")
+            print(f"Valley: {valley}")
+            print(f"Valleys: {valleys}")
         # Find the valley containing the target sector
         selected_valley = None
 
         for valley in valleys:
-            if min(valley) <= target_sector <= max(valley) and len(valley) >= 4:  # check condition and how the valley is picked
+            # check condition and how the valley is picked
+            if min(valley) <= target_sector <= max(valley) and len(valley) >= 4:
                 selected_valley = valley
                 print(f"Selected Valley: {selected_valley}")
                 break
 
-        if selected_valley:
+        if selected_valley:##
             k_near = min(selected_valley)
             k_far = k_near + self.s_max
+            prob = k_near + self.s_max
             if len(selected_valley) <= self.s_max:
                 # print("Valley is narrow")
                 k_far = max(selected_valley)
+            if len(selected_valley) >= prob:
+                k_far = max(selected_valley)
             theta = (k_near + k_far) / 2
+            if len(selected_valley) >= prob and target_sector in selected_valley:
+                theta = target_sector
             best_sector = int(theta)
             theta_deg = best_sector * self.sector_angle
-            # print(f"Target angle: {target_angle_deg:.2f}° => Sector {target_sector}")
+            print(f"Target angle: {target_angle_deg:.2f}° => Sector {target_sector}")
             # print(f"Steering Valley: {selected_valley}")
-            # print(f"k_near: {k_near}, k_far: {k_far}, Steering direction sector: {theta}")
-            # print(f"Steering angle: {theta_deg:.2f}°")
+            print(f"k_near: {k_near}, k_far: {k_far}, Steering direction sector: {theta}")
+            print(f"Steering angle: {theta_deg:.2f}°")
         # Start searching for adjacent free valley
         else:
             total_sectors = len(polar_histogram)
-            # print("Target point is behind obstacle, choosing largest adjacent Valley")
+            # TODO: look into which cases wrongfully trigger the search algo
+            print("Target point is behind obstacle, choosing largest adjacent Valley")
             # check whether target sector is adjacent to any valleys, then check which of the two is larger
             nearby_sectors = set((target_sector + offset) % total_sectors for offset in range(-2, 2))
             # print(f"Nearby sectors: {nearby_sectors}")
@@ -215,9 +212,10 @@ class VectorFieldHistogram:
                 if not set(valley).isdisjoint(nearby_sectors) and len(valley) >= 4:
                     candidate_valleys.append(valley)
             # candidate valley has been found in first iteration
-            if candidate_valleys:
+            # check in what sector human_point is and pick valley dependent on which has the human_sector
+            if candidate_valleys and len(candidate_valleys) >= 2:
                 selected_valley = max(candidate_valleys, key=len)
-                # print(f"Selected adjacent Valley: {selected_valley}")
+                print(f"Selected adjacent Valley: {selected_valley}")
                 k_near = min(selected_valley)
                 k_far = k_near + self.s_max
                 if len(selected_valley) <= self.s_max:
@@ -276,16 +274,17 @@ class VectorFieldHistogram:
 
         # calculation of directional vector
         if theta_deg is not None:
-            theta_rad = np.radians(-(theta_deg - 120.0)) # -- maybe np. instead of .math?
+            theta_rad = np.radians(120.0 - theta_deg)  # -- maybe np. instead of .math?
             direction_vector = np.array([np.cos(theta_rad), np.sin(theta_rad), 0])
+            print(theta_rad)
             print(f"Directional Vector: {direction_vector}")
             print("---------------------------------------")
             self.direction_vector = direction_vector
             return direction_vector
         else:
             direction_vector = np.array([0.0, 0.0, 0.0])
-            # print(f"Directional Vector: {direction_vector}")
-            # print("---------------------------------------")
+            print(f"Directional Vector: {direction_vector}")
+            print("---------------------------------------")
             self.direction_vector = direction_vector
             return direction_vector
         # benchmark stuff
@@ -317,52 +316,39 @@ class VectorFieldHistogram:
     #
     #     return smoothed
     # ------------------- ONLY USE THE PLOT FOR TESTING ---------------------
-    # def histogram_plot(self, target_point):
-    #     # Start of plot gen
-    #     fig = plt.figure(figsize=(12, 6))
-    #     ax0 = fig.add_subplot(1, 2, 1)
-    #     ax1 = fig.add_subplot(1, 2, 2, polar=True)
-    #
-    #     # LIDAR PointCloud
-    #     ax0.set_title('LIDAR Map')
-    #     ax0.scatter(-self.y_points, self.x_points, c='blue', s=1, label='LIDAR points')
-    #     ax0.plot(-target_point[1], target_point[0], 'mx', markersize=10)
-    #     ax0.plot(-self.robot_position[1], self.robot_position[0], 'go', markersize=10, label='Robot Position')
-    #     ax0.set_xlim(-self.max_range, self.max_range)
-    #     ax0.set_ylim(-self.max_range, self.max_range)
-    #     ax0.set_aspect('equal')
-    #     ax0.grid(True)
-    #     ax0.legend()
-    #
-    #     # Polar Histogram
-    #     ax1.set_title('Polar Histogram')
-    #     ax1.set_theta_direction(-1)
-    #     ax1.set_theta_zero_location('W', 30)
-    #     colors = ['green' if self.polar_histogram[i] < self.obstacle_threshold else 'red'
-    #               for i in range(len(self.polar_histogram))]
-    #     angles_rad = np.deg2rad(np.arange(0, 240, self.sector_angle))
-    #     ax1.bar(angles_rad, self.polar_histogram, width=np.deg2rad(self.sector_angle), bottom=0.0, align='edge',
-    #             color=colors)
-    #     ax1.plot('m--', linewidth=0.5, label='sectors')  # needed to portray legend for graph accurately
-    #     for angles in angles_rad:
-    #         ax1.plot([angles, angles], [0, np.max(self.polar_histogram)], 'm--', linewidth=0.5)
-    #     if self.theta_deg is not None:
-    #         best_rad = np.deg2rad(self.theta_deg)
-    #         ax1.plot([best_rad, best_rad], [0, np.max(self.polar_histogram)], 'b--', linewidth=2,
-    #                  label='best direction')
-    #     ax1.legend(loc='upper right')
-    #     plt.tight_layout()
-    #     plt.savefig('/home/yannis/Documents/vfh.png')
+    def histogram_plot(self, target_point):
+        # Start of plot gen
+        fig = plt.figure(figsize=(12, 6))
+        ax0 = fig.add_subplot(1, 2, 1)
+        ax1 = fig.add_subplot(1, 2, 2, polar=True)
 
+        # LIDAR PointCloud
+        ax0.set_title('LIDAR Map')
+        ax0.scatter(-self.y_points, self.x_points, c='blue', s=1, label='LIDAR points')
+        ax0.plot(-target_point[1], target_point[0], 'mx', markersize=10)
+        ax0.plot(-self.robot_position[1], self.robot_position[0], 'go', markersize=10, label='Robot Position')
+        ax0.set_xlim(-self.max_range, self.max_range)
+        ax0.set_ylim(-self.max_range, self.max_range)
+        ax0.set_aspect('equal')
+        ax0.grid(True)
+        ax0.legend()
 
-# if __name__ == '__main__':
-#     rospy.init_node('vfh_node')
-#     vfh = VectorFieldHistogram(num_readings=240,
-#                                max_range=5.0,
-#                                grid_size=0.1,
-#                                sector_angle=5,
-#                                obstacle_threshold=8,
-#                                s_max=12,
-#                                input_topic="/hsrb/base_scan")
-#
-#     rospy.spin()
+        # Polar Histogram
+        ax1.set_title('Polar Histogram')
+        ax1.set_theta_direction(-1)
+        ax1.set_theta_zero_location('W', 30)
+        colors = ['green' if self.polar_histogram[i] < self.obstacle_threshold else 'red'
+                  for i in range(len(self.polar_histogram))]
+        angles_rad = np.deg2rad(np.arange(0, 240, self.sector_angle))
+        ax1.bar(angles_rad, self.polar_histogram, width=np.deg2rad(self.sector_angle), bottom=0.0, align='edge',
+                color=colors)
+        ax1.plot('m--', linewidth=0.5, label='sectors')  # needed to portray legend for graph accurately
+        for angles in angles_rad:
+            ax1.plot([angles, angles], [0, np.max(self.polar_histogram)], 'm--', linewidth=0.5)
+        if self.theta_deg is not None:
+            best_rad = np.deg2rad(self.theta_deg)
+            ax1.plot([best_rad, best_rad], [0, np.max(self.polar_histogram)], 'b--', linewidth=2,
+                     label='best direction')
+        ax1.legend(loc='upper right')
+        plt.tight_layout()
+        plt.savefig('/home/suturo/Documents/vfh.png')
